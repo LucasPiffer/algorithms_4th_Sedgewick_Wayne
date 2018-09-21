bb2672531e8a87d3c925e2c2ea0ff04fbcf42cc7
LucasPiffer <piffer.lucas@gmail.com>
2018-09-20 22:20:10 -0300

    binary_search

--- fundamentals_bythebook.rb
+++ fundamentals_bythebook.rb
@@ -5,3 +5,28 @@ def gcd(p, q)
 end
 
 puts gcd(54, 24)
+
+def binary_search(data_set, to_find)
+  lo = 0
+  hi = data_set.size - 1
+  
+  while(lo <= hi)
+    mid = lo + (hi - lo) / 2
+
+    if(to_find > data_set[mid])
+      lo = mid + 1
+    elsif(to_find < data_set[mid])
+      hi = mid - 1
+    else
+      return mid
+    end
+  end 
+
+  -1
+
+end
+
+data_set = [1, 2, 5, 10]
+puts binary_search(data_set, 5)
+
+
