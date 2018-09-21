9c4a7e3a73e5e162051e06a3d4eb87b16259b0e2
LucasPiffer <piffer.lucas@gmail.com>
2018-09-20 22:24:35 -0300

    Fix out of bound for binary search

--- fundamentals.py
+++ fundamentals.py
@@ -20,19 +20,20 @@ print(gcd(8, 12))
 def binary_search(data_set, to_find_number, p, r):
  middle = (p + r) / 2
 
- if(p > r):
-   return False
+ if(middle >= len(data_set) or middle < 0):
+   return -1
  
  if(data_set[middle] > to_find_number ):
    return binary_search(data_set, to_find_number, 0, middle - 1) 
  elif(data_set[middle] < to_find_number):
    return binary_search(data_set, to_find_number, middle + 1, r)
  else:
-  return (to_find_number == data_set[middle]) 
+  return middle 
 
 
 data_set = [3, 5, 10 , 20]
 print(binary_search(data_set, 20, 0, len(data_set)))
+#print(binary_search(data_set, 40, 0, len(data_set)))
 
 
 
