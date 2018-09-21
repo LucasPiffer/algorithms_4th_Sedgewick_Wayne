93503ffa51f107a8ed7e52a11d83d2b91bde4ed1
LucasPiffer <piffer.lucas@gmail.com>
2018-09-20 21:54:27 -0300

    Binary search

--- fundamentals.py
+++ fundamentals.py
@@ -15,3 +15,29 @@ def gcd(dividendo, divisor):
     return divisor
 
 print(gcd(8, 12))
+
+
+def binary_search(data_set, to_find_number, p, r):
+ middle = (p + r) / 2
+
+ if(p > r):
+   return False
+ 
+ if(data_set[middle] > to_find_number ):
+   return binary_search(data_set, to_find_number, 0, middle - 1) 
+ elif(data_set[middle] < to_find_number):
+   return binary_search(data_set, to_find_number, middle + 1, r)
+ else:
+  return (to_find_number == data_set[middle]) 
+
+
+data_set = [3, 5, 10 , 20]
+print(binary_search(data_set, 20, 0, len(data_set)))
+
+
+
+
+
+
+
+
